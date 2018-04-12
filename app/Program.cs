3bca8d81686a57a08a98b5e0906e5713b0646f07
Thiago Cordeiro <thiago6891@gmail.com>
2018-04-12 08:46:51 -0300

    refactoring and file restructuring

--- app/Program.cs
+++ app/Program.cs
@@ -1,23 +1,36 @@
-﻿using Utils.FileProcessing;
+﻿using System;
+using System.IO;
+using Utils.FileProcessing;
 
 namespace app
 {
     class Program
     {
+        static readonly string INPUT_DIR = "input";
+        static readonly string OUTPUT_DIR = "output";
+        static readonly string INPUT_FILE_ONE = "matriculasSemDV.txt";
+        static readonly string INPUT_FILE_TWO = "matriculasParaVerificar.txt";
+        static readonly string OUTPUT_FILE_ONE = "matriculasComDV.txt";
+        static readonly string OUTPUT_FILE_TWO = "matriculasVerificadas.txt";
+
         static void Main(string[] args)
         {
+            Directory.CreateDirectory(OUTPUT_DIR);
+
             var fileProcessors = new FileProcessor[] {
                 new DigitGenerationFileProcessor(
-                    "matriculasSemDV.txt",
-                    "matriculasComDV.txt"
+                    INPUT_DIR + "/" + INPUT_FILE_ONE,
+                    OUTPUT_DIR + "/" + OUTPUT_FILE_ONE
                 ),
                 new DigitVerificationFileProcessor(
-                    "matriculasParaVerificar.txt",
-                    "matriculasVerificadas.txt"
+                    INPUT_DIR + "/" + INPUT_FILE_TWO,
+                    OUTPUT_DIR + "/" + OUTPUT_FILE_TWO
                 )
             };
 
             foreach (var fp in fileProcessors) fp.ProcessFile();
+
+            Console.WriteLine("Done! Results can be found in the /output directory.");
         }
     }
 }
